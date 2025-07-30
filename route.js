72663ffbb8dfd0459affa6b35656fe1f9cebbd4b
yousra-zaheer <yousrazaheerOfficial@gmail.com>
2025-07-30 04:31:01 -0700

    route.js

--- route.js
+++ route.js
@@ -3,6 +3,7 @@ import { userlogin, usersignup } from './controller.js';
 
 const router=express.Router();
 
+//router-level middleware handling
 router.get('/login',userlogin)
 router.get('/signup',usersignup)
 
